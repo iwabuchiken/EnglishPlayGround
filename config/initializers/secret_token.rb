9c9275a6c9f1ec910245303432bb7a821cbc28c6
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-05-20 01:36:43 +0900

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EnglishPlayGround::Application.config.secret_token = '0f319c44cacbe1b7aed7e6a2bbc90f1ff2630fabd6b1212deff38739f891cc293e64b827414cce87d793f80bb85777c3f94552c0b2f78964c1cacf410b77157d'
